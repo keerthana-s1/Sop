e4d19744d3fe88fc5d14077758b508b19621be38
keerthana-s1 <63349790+keerthana-s1@users.noreply.github.com>
2020-05-07 23:10:47 +0530

    Update Np.cpp

--- Np.cpp
+++ Np.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+//the first array is modified such that a[i] contains the number of pizza boxes used till the day i+1 and not just on the day.
+//the for each query, the least day containing boxes greater and or equal to the required number is found and displayed.
 int main()
 {
     long n,q,j=0,sum=0,i,temp,s=0;
-    //long a[1000];
+   
 
     cin>>n>>q;
     vector<long> a;
+  
     for(i=0;i<n;i++)
     {   cin>>temp;
         sum=temp+s;
@@ -17,7 +20,7 @@ int main()
     sum=0;
     for(i=0;i<q;i++)
     {   cin>>temp;
-        //b.push_back(temp);
+        
         for(j=0;j<n;j++)
         {
             if(temp<=a[j])
